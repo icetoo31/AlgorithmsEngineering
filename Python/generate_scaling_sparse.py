ddc174d7aae4c66608483b5933d4b7bed2c116ae
MartinKaraivanov <martikaraivanov@gmail.com>
2025-12-07 18:06:14 +0100

    bigger step and more graphs per step

--- Python/generate_scaling_sparse.py
+++ Python/generate_scaling_sparse.py
@@ -4,24 +4,21 @@ import os
 # ---------------- CONFIGURATION ----------------
 
 OUTPUT_DIR = "IO"
-OUTPUT_FILE = "sparse_scaling_1_to_500.txt"
+OUTPUT_FILE = "big_sparse_scaling_10_to_1000.txt"
 
 SEED = 42
-MAX_N = 500
 
 W_MIN = 1
 W_MAX = 100
 
-EDGE_FACTOR = 3  # sparse: m = 3n
+EDGE_FACTOR = 3      # sparse: m = 3n
+GRAPHS_PER_SIZE = 100
 
 random.seed(SEED)
 
 # -----------------------------------------------
 
 def sparse_graph(n):
-    if n <= 1:
-        return n, []
-
     m = min(EDGE_FACTOR * n, n * (n - 1))
     edges = set()
 
@@ -39,20 +36,19 @@ def generate():
     path = os.path.join(OUTPUT_DIR, OUTPUT_FILE)
 
     with open(path, "w") as f:
-        ##f.write(f"{MAX_N}\n")
+        for n in range(10, 1001, 10):
+            for _ in range(GRAPHS_PER_SIZE):
+                n_nodes, edges = sparse_graph(n)
 
-        for n in range(2, MAX_N + 1):
-            n_nodes, edges = sparse_graph(n)
+                f.write(f"{n_nodes} {len(edges)}\n")
 
-            f.write(f"{n_nodes} {len(edges)}\n")
+                for i, (u, v, w) in enumerate(edges):
+                    if i == len(edges) - 1:
+                        f.write(f"{u} {v} {w},\n")
+                    else:
+                        f.write(f"{u} {v} {w}\n")
 
-            for i, (u, v, w) in enumerate(edges):
-                if i == len(edges) - 1:
-                    f.write(f"{u} {v} {w},\n")
-                else:
-                    f.write(f"{u} {v} {w}\n")
-            
-            f.write(f"\n")
+                f.write("\n")
 
     print(f"Generated {path}")
 
